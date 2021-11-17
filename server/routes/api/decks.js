4028e4bed8133ecefe5f88193cb51e8a1c820940
John Basile <ashton.basile@gmail.com>
2021-11-17 12:20:42 +0100

    Loading config variables only on develop

--- server/routes/api/decks.js
+++ server/routes/api/decks.js
@@ -7,13 +7,16 @@ const router = express.Router();
 
 const deckSchema = require('../../models/deckSchema.js');
 
-const dotenv = require("dotenv");
+if (process.env.NODE_ENV !== "production") {
+    const dotenv = require("dotenv");
 
-const result = dotenv.config();
+    const result = dotenv.config();
 
-if (result.error){
-    throw result.error;
+    if (result.error){
+        throw result.error;
+    }
 }
+
 
 const url = process.env.mongoURL;
 mongoose.connect(url,{
