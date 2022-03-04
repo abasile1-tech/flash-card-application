2ad1e89fb0a04b6834744e83a5734e092c696ea8
John <ashton.basile@gmail.com>
2022-03-04 14:00:07 +0100

    Cleaned up some code that was no longer needed and adjusted some formatting of spaces to tabs.

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,7 +3,7 @@ import SingleDeck from './views/SingleDeck.vue';
 import LoginPage from './views/LoginPage.vue';
 
 export default [
-    {path:'/welcome/',component:Welcome, props:true},
-    {path:'/welcome/single-deck/:deckName',component:SingleDeck,props:true},
-    {path:'/',component:LoginPage, props:true}
+	{path:'/welcome/',component:Welcome, props:true},
+	{path:'/welcome/single-deck/:deckName',component:SingleDeck,props:true},
+	{path:'/',component:LoginPage, props:true}
 ]
