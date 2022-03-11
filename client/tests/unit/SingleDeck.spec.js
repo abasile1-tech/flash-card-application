e5780a8e949994de7d2e5a172ac19d7449db3a81
John <ashton.basile@gmail.com>
2022-03-11 14:31:18 +0100

    I added some more automated tests for the cardsListIndex in SingleDeck.

--- client/tests/unit/SingleDeck.spec.js
+++ client/tests/unit/SingleDeck.spec.js
@@ -6,28 +6,35 @@ describe('Testing SingleDeck Component Methods', () => {
         propsData: {
             emittedObject: {
                 _id: "10938497987519739874", //fake id
-                cards: [{cardFront:"spam",cardBack:"hool"},{cardFront:"neo",cardBack:"goo"}],
+                cards: [{cardFront:"spam",cardBack:"hool"},{cardFront:"neo",cardBack:"goo"},{cardFront:"foo",cardBack:"bar"}],
                 deckName: "Javi"
             }
         }
     });
 
     // cardsListIndex is 0 now
-    it('correctly updates the index by 1 when updateCardIndex is called', () => {
+    it('correctly updates the index by 1 when updateCardIndex(1) is called', () => {
         wrapper.vm.updateCardIndex(1);  
         expect(wrapper.vm.cardsListIndex).toBe(1); 
     })
     // cardsListIndex is 1 now
-    it('correctly updates the index by -1 when updateCardIndex is called', () => {
+    it('correctly updates the index by -1 when updateCardIndex(-1) is called', () => {
         wrapper.vm.updateCardIndex(-1);  
         expect(wrapper.vm.cardsListIndex).toBe(0); 
     })
 
     // cardsListIndex is 0 now
-    it('correctly updates the index by 100 when updateCardIndex is called', () => {
+    it('correctly updates the index by -2 when updateCardIndex(-1) is called twice', () => {
+        wrapper.vm.updateCardIndex(-1); 
+        wrapper.vm.updateCardIndex(-1);
+        expect(wrapper.vm.cardsListIndex).toBe(1); 
+    })
+
+    it('correctly updates the index by 2 when updateCardIndex(1) is called twice', () => {
+        wrapper.vm.updateCardIndex(1); 
         wrapper.vm.updateCardIndex(1);
-        wrapper.vm.updateCardIndex(1);  
         expect(wrapper.vm.cardsListIndex).toBe(0); 
     })
+
 
 })
