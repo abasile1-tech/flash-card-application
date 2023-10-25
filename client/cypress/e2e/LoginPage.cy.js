305a07166a62e847148ea7fbc68d7778af5d2ddc
John <ashton.basile@gmail.com>
2023-10-25 15:20:25 +0100

    streamlining by adding getByData command

--- client/cypress/e2e/LoginPage.cy.js
+++ client/cypress/e2e/LoginPage.cy.js
@@ -1,8 +1,6 @@
 describe("Login Page", () => {
   it("the h1 contains the correct text", () => {
     cy.visit("http://localhost:8080/#/");
-    cy.get("[data-testid='login-h1']").contains(
-      "Welcome to the Flash Card App!"
-    );
+    cy.getByData("login-h1").contains("Welcome to the Flash Card App!");
   });
 });
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("getByData", (selector) => {
+  return cy.get(`[data-testid=${selector}]`);
+});
