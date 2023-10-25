b713cfe95973b2f1a70bcdfdf354ede6533777a9
John <ashton.basile@gmail.com>
2023-10-25 15:09:57 +0100

    added the data-testid convention for cypress testing

--- client/cypress/e2e/LoginPage.cy.js
+++ client/cypress/e2e/LoginPage.cy.js
@@ -1,6 +1,8 @@
 describe("Login Page", () => {
   it("the h1 contains the correct text", () => {
-    cy.visit("http://localhost:5000/#/");
-    cy.get("h1").contains("Welcome to the Flash Card App!");
+    cy.visit("http://localhost:8080/#/");
+    cy.get("[data-testid='login-h1']").contains(
+      "Welcome to the Flash Card App!"
+    );
   });
 });
