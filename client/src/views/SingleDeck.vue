2b046df1e44e4e8469eac44a7917feac1b3010a9
John <ashton.basile@gmail.com>
2023-11-01 16:03:17 +0000

    I added an algorithm for comparing the similarity of the two strings

--- client/src/views/SingleDeck.vue
+++ client/src/views/SingleDeck.vue
@@ -217,9 +217,9 @@
       />
 
       <div id="cardButtonsDiv">
-        <em v-if="!this.isListeningForSpeech && this.speechInputResult != ''">{{
-          this.speechScore
-        }}</em>
+        <em v-if="!this.isListeningForSpeech && this.speechInputResult != ''"
+          >{{ this.speechScore }}% Correct</em
+        >
         <select id="selectBlock" v-if="!isMobile" v-model="selectedLanguage">
           <option disabled value="">Please select a language:</option>
           <option
@@ -617,12 +617,52 @@ export default {
       this.confidence = 0;
       this.speechScore = 0;
     },
-    calculateSpeechScore() {
-      if (this.cardPrompt == this.speechInputResult) {
-        this.speechScore = 100;
-      } else {
-        this.speechScore = 0;
+    editDistance(s1, s2) {
+      s1 = s1.toLowerCase();
+      s2 = s2.toLowerCase();
+
+      var costs = new Array();
+      for (var i = 0; i <= s1.length; i++) {
+        var lastValue = i;
+        for (var j = 0; j <= s2.length; j++) {
+          if (i == 0) costs[j] = j;
+          else {
+            if (j > 0) {
+              var newValue = costs[j - 1];
+              if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                newValue =
+                  Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+              costs[j - 1] = lastValue;
+              lastValue = newValue;
+            }
+          }
+        }
+        if (i > 0) costs[s2.length] = lastValue;
       }
+      return costs[s2.length];
+    },
+    calculateSimilarity(s1, s2) {
+      var longer = s1;
+      var shorter = s2;
+      if (s1.length < s2.length) {
+        longer = s2;
+        shorter = s1;
+      }
+      var longerLength = longer.length;
+      if (longerLength == 0) {
+        return 1.0;
+      }
+      return (
+        (longerLength - this.editDistance(longer, shorter)) /
+        parseFloat(longerLength)
+      );
+    },
+    calculateSpeechScore() {
+      const similarityResult = this.calculateSimilarity(
+        this.cardPrompt,
+        this.speechInputResult
+      );
+      this.speechScore = similarityResult * 100;
     },
     getSpeechInput() {
       const recognition = new SpeechRecognition();
